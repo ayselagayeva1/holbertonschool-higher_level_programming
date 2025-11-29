526693b4e92e04203afd24adfee402eb168a248a
Aayselagayeva1 <ayselagayevaa13@gmail.com>
2025-11-29 18:26:27 +0000

    update

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function to return dictionary description of an object for JSON
+serialization"""
+
+
+def class_to_json(obj):
+    """Return dictionary of obj attributes for JSON serialization"""
+    return obj.__dict__.copy()
