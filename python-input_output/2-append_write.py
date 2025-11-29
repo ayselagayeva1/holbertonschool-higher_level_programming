b9793f418a6b60c4dc4d6a6335f58c418ed545c5
Aayselagayeva1 <ayselagayevaa13@gmail.com>
2025-11-29 18:21:07 +0000

    update

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,13 +2,13 @@
 """
 2-append_write module
 
-Appends a UTF-8 string to a text file and returns the number of characters added.
+Appendsto a text file and returns the number of characters added.
 """
 
 
 def append_write(filename="", text=""):
     """
-    Appends a string to a UTF-8 text file and returns the number of characters added.
+    Appto a UTF-8 text file and returns the number of characters added.
     """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
