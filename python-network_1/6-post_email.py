8f5417bbd16c9cb61c90b1be696ae9dcb90617bd
ayselagayeva1 <ayselagayevaa13@gmail.com>
2025-12-06 17:30:14 +0000

    .

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email, sends a POST request to the URL
-with the email as a parameter, and displays the body of the response.
+Sends a POST request with an email parameter to a given URL
 """
 
 import sys
 import requests
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
     data = {"email": email}
+
     response = requests.post(url, data=data)
 
     print(response.text)
